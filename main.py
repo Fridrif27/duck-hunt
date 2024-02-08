17cf531959e582ebb3a742d390a462d07dd25bc4
Pavlo <bohushpavlo@gmail.com>
2024-02-08 19:43:52 +0200

    Created sinusoidal motion

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import pygame
 import math
 
 class Target:
-    def __init__(self, x, y, speed, width):
+    def __init__(self, x, y, radius, speed, amplitude_y, frequency_y):
         self.x = x
         self.y = y
-        self.radius = 20
+        self.radius = radius
         self.speed = speed
-        self.width = width
-    def move(self, dx):
-        self.x += dx
+        self.amplitude_y = amplitude_y
+        self.frequency_y = frequency_y
+        self.initial_y = y
+    def move(self):
+        self.x += self.speed
+        self.y = self.amplitude_y * math.sin(self.frequency_y * self.x) + self.initial_y
         if self.x - self.radius > self.width:
             self.x = -self.radius
         elif self.x + self.radius < 0:
@@ -23,8 +26,15 @@ class DuckHuntGame:
         self.WIDTH = width
         self.HEIGHT = height
         self.screen = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
-        target_coordinates_and_speed = [((100, 200), -1), ((300, 400), 1), ((500, 100), -2), ((700, 300), 1), ((800, 600), -3)]
-        self.targets = [Target(x, y, speed, self.WIDTH) for (x, y), speed in target_coordinates_and_speed]
+        self.targets = [
+            Target(100, 200, 20, 1, 100, 0.02),
+            Target(300, 400, 20, -1, 50, 0.03),
+            Target(500, 100, 20, 2, 150, 0.01),
+            Target(700, 300, 20, -1.5, 10, 0.015),
+            Target(800, 600, 20, 1.5, 125, 0.025)
+        ]
+        for target in self.targets:
+            target.width = self.WIDTH
         self.background_image = pygame.transform.scale(pygame.image.load("bgs1.png").convert(), (self.WIDTH, self.HEIGHT))
 
     def run_game(self):
@@ -45,7 +55,7 @@ class DuckHuntGame:
             self.screen.blit(self.background_image, (0, 0))
             for target in self.targets:
                 pygame.draw.circle(self.screen, (255, 0, 0), (target.x, target.y), target.radius)
-                target.move(target.speed)
+                target.move()
             pygame.display.flip()
 
         pygame.quit()
