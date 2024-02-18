f829d82d0fe9b8f9e0ae72faaf2ec77b92aa9d24
Pavlo <bohushpavlo@gmail.com>
2024-02-19 00:56:51 +0200

    The code is optimized

--- src/main.py
+++ src/main.py
@@ -56,37 +56,22 @@ class DuckHuntGame:
         self.load_background()
 
     def load_images(self):
-        self.bird_images = [
-            pygame.image.load("assets/targets/bird1.png").convert_alpha(),
-            pygame.image.load("assets/targets/bird2.png").convert_alpha(),
-            pygame.image.load("assets/targets/bird3.png").convert_alpha(),
-            pygame.image.load("assets/targets/bird4.png").convert_alpha(),
-        ]
-        transparent_color = (0, 0, 0)
-        for image in self.bird_images:
-            image.set_colorkey(transparent_color)
-        self.bird_images = [pygame.transform.scale(image, (40, 40)) for image in self.bird_images]
-
+        self.bird_images = [pygame.transform.scale(pygame.image.load(f"assets/targets/bird{i}.png").convert_alpha(), (40, 40)) for i in range(1, 5)]
+    
     def load_sounds(self):
-        self.sound_shot = pygame.mixer.Sound("assets/sounds/shot.mp3")
-        self.sound_shot.set_volume(0.08)
-        self.sound_bird1 = pygame.mixer.Sound("assets/sounds/bird1.mp3")
-        self.sound_bird2 = pygame.mixer.Sound("assets/sounds/bird2.mp3")
-        self.sound_bird3 = pygame.mixer.Sound("assets/sounds/bird3.mp3")
-        self.sound_bird4 = pygame.mixer.Sound("assets/sounds/bird4.mp3")
+        sound_files = ["shot.mp3", "bird1.mp3", "bird2.mp3", "bird3.mp3", "bird4.mp3"]
+        self.sounds = {file.split(".")[0]: pygame.mixer.Sound(f"assets/sounds/{file}") for file in sound_files}
+        self.sounds["shot"].set_volume(0.08)
+
 
     def load_background(self):
         self.background_image = pygame.transform.scale(pygame.image.load("assets/bgs/bgs1.png").convert(), (self.WIDTH, self.HEIGHT))
 
     def initialize_targets(self):
         level = self.levels[self.current_level]
-        self.targets = [
-            Target(100, 200, 20, 1, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH),
-            Target(300, 400, 20, -1, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH),
-            Target(500, 100, 20, 2, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH),
-            Target(700, 300, 20, -1.5, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH),
-            Target(800, 600, 20, 1.5, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH)
-        ]
+        positions = [(100, 200), (300, 400), (500, 100), (700, 300), (800, 600)]
+        speeds = [1, -1, 2, -1.5, 1.5]
+        self.targets = [Target(pos[0], pos[1], 20, speed, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH) for pos, speed in zip(positions, speeds)]
 
     def run_game(self):
         run = True
@@ -106,24 +91,17 @@ class DuckHuntGame:
 
     def handle_shooting(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        self.sound_shot.play()
+        self.sounds["shot"].play()
         for target in self.targets:
             if (
                 target.x - target.radius <= mouse_x <= target.x + target.radius and
                 target.y - target.radius <= mouse_y <= target.y + target.radius
             ):
                 self.targets.remove(target)
-                self.handle_bird_sound(target)
+                bird_sounds = [self.sounds[f"bird{i+1}"] for i in range(4)]
+                bird_sound = bird_sounds[self.bird_images.index(target.current_bird_image)]
+                bird_sound.play()
 
-    def handle_bird_sound(self, target):
-        if target.current_bird_image == self.bird_images[0]:
-            self.sound_bird1.play()
-        elif target.current_bird_image == self.bird_images[1]:
-            self.sound_bird2.play()
-        elif target.current_bird_image == self.bird_images[2]:
-            self.sound_bird3.play()
-        elif target.current_bird_image == self.bird_images[3]:
-            self.sound_bird4.play()
 
     def update_screen(self):
         self.screen.blit(self.background_image, (0, 0))
