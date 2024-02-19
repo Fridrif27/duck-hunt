8f5493997527a63ea20f520359271c8fff368807
Polina <polina_kupreychuk@knu.ua>
2024-02-19 18:27:38 +0200

    pre-commit created

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pygame
 import math
 import random
 
+
 class Level:
     def __init__(self, bird_speed, amplitude_y, frequency_y):
         self.bird_speed = bird_speed
@@ -10,7 +11,17 @@ class Level:
 
 
 class Target:
-    def __init__(self, x, y, radius, speed, amplitude_y, frequency_y, bird_images, screen_width):
+    def __init__(
+        self,
+        x,
+        y,
+        radius,
+        speed,
+        amplitude_y,
+        frequency_y,
+        bird_images,
+        screen_width,
+    ):
         self.x = x
         self.y = y
         self.radius = radius
@@ -24,7 +35,10 @@ class Target:
 
     def move(self):
         self.x += self.speed
-        self.y = self.amplitude_y * math.sin(self.frequency_y * self.x) + self.initial_y
+        self.y = (
+            self.amplitude_y * math.sin(self.frequency_y * self.x)
+            + self.initial_y
+        )
         self.check_boundary()
 
     def check_boundary(self):
@@ -32,6 +46,7 @@ class Target:
             self.x = -self.radius
         elif self.x + self.radius < 0:
             self.x = self.screen_width + self.radius
+
 
 class DuckHuntGame:
     def __init__(self, width, height):
@@ -45,7 +60,7 @@ class DuckHuntGame:
         self.levels = [
             Level(bird_speed=1, amplitude_y=0, frequency_y=0),
             Level(bird_speed=1.5, amplitude_y=150, frequency_y=0.03),
-            Level(bird_speed=2, amplitude_y=200, frequency_y=0.04)
+            Level(bird_speed=2, amplitude_y=200, frequency_y=0.04),
         ]
         self.load_assets()
         self.initialize_targets()
@@ -57,10 +72,22 @@ class DuckHuntGame:
 
     def load_images(self):
         self.bird_images = [
-            pygame.transform.scale(pygame.image.load("assets/targets/bird1.png").convert(), (40, 40)),
-            pygame.transform.scale(pygame.image.load("assets/targets/bird2.png").convert(), (40, 40)),
-            pygame.transform.scale(pygame.image.load("assets/targets/bird3.png").convert(), (40, 40)),
-            pygame.transform.scale(pygame.image.load("assets/targets/bird4.png").convert(), (40, 40)),
+            pygame.transform.scale(
+                pygame.image.load("assets/targets/bird1.png").convert(),
+                (40, 40),
+            ),
+            pygame.transform.scale(
+                pygame.image.load("assets/targets/bird2.png").convert(),
+                (40, 40),
+            ),
+            pygame.transform.scale(
+                pygame.image.load("assets/targets/bird3.png").convert(),
+                (40, 40),
+            ),
+            pygame.transform.scale(
+                pygame.image.load("assets/targets/bird4.png").convert(),
+                (40, 40),
+            ),
         ]
 
     def load_sounds(self):
@@ -72,16 +99,64 @@ class DuckHuntGame:
         self.sound_bird4 = pygame.mixer.Sound("assets/sounds/bird4.mp3")
 
     def load_background(self):
-        self.background_image = pygame.transform.scale(pygame.image.load("assets/bgs/bgs1.png").convert(), (self.WIDTH, self.HEIGHT))
+        self.background_image = pygame.transform.scale(
+            pygame.image.load("assets/bgs/bgs1.png").convert(),
+            (self.WIDTH, self.HEIGHT),
+        )
 
     def initialize_targets(self):
         level = self.levels[self.current_level]
         self.targets = [
-            Target(100, 200, 20, 1, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH),
-            Target(300, 400, 20, -1, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH),
-            Target(500, 100, 20, 2, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH),
-           Target(700, 300, 20, -1.5, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH),
-            Target(800, 600, 20, 1.5, level.amplitude_y, level.frequency_y, self.bird_images, self.WIDTH)
+            Target(
+                100,
+                200,
+                20,
+                1,
+                level.amplitude_y,
+                level.frequency_y,
+                self.bird_images,
+                self.WIDTH,
+            ),
+            Target(
+                300,
+                400,
+                20,
+                -1,
+                level.amplitude_y,
+                level.frequency_y,
+                self.bird_images,
+                self.WIDTH,
+            ),
+            Target(
+                500,
+                100,
+                20,
+                2,
+                level.amplitude_y,
+                level.frequency_y,
+                self.bird_images,
+                self.WIDTH,
+            ),
+            Target(
+                700,
+                300,
+                20,
+                -1.5,
+                level.amplitude_y,
+                level.frequency_y,
+                self.bird_images,
+                self.WIDTH,
+            ),
+            Target(
+                800,
+                600,
+                20,
+                1.5,
+                level.amplitude_y,
+                level.frequency_y,
+                self.bird_images,
+                self.WIDTH,
+            ),
         ]
 
     def run_game(self):
@@ -105,8 +180,10 @@ class DuckHuntGame:
         self.sound_shot.play()
         for target in self.targets:
             if (
-                target.x - target.radius <= mouse_x <= target.x + target.radius and
-                target.y - target.radius <= mouse_y <= target.y + target.radius
+                target.x - target.radius <= mouse_x <= target.x + target.radius
+                and target.y - target.radius
+                <= mouse_y
+                <= target.y + target.radius
             ):
                 self.targets.remove(target)
                 self.handle_bird_sound(target)
@@ -125,7 +202,12 @@ class DuckHuntGame:
         self.screen.blit(self.background_image, (0, 0))
         for target in self.targets:
             target.move()
-            self.screen.blit(target.current_bird_image, target.current_bird_image.get_rect(center=(int(target.x), int(target.y))))
+            self.screen.blit(
+                target.current_bird_image,
+                target.current_bird_image.get_rect(
+                    center=(int(target.x), int(target.y))
+                ),
+            )
         pygame.display.flip()
 
     def check_game_status(self):
@@ -141,6 +223,7 @@ class DuckHuntGame:
                 pygame.time.delay(1000)
                 return False
         return True
+
 
 if __name__ == "__main__":
     game = DuckHuntGame(900, 800)
